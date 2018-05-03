d514bc56fc28074ce73ecedd0536a2d01f87e3d2
Bartosz Górka <bartosz.gorka@appunite.com>
2018-05-03 15:48:50 +0200

    Vessels recognition

---
+++ Blood_vessels_recognition/Vessels_recognition.py
@@ -0,0 +1,15 @@
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO LIST
+# * Read image
+# * Read expert mask
+# * Select only green channel from image
+# * Dilatation
+# * MedianBlur
+# * Save result as picture
+# * Machine Learning with SciKit
---
+++ README.md
@@ -0,0 +1,7 @@
+# Medical Informatics
+Study projects - PUT Poznań 2018  
+
+## Projects
+  1. [Project 1 - Tomograph simulation](../master/Tomograph)
+  2. [Project 2 - Blood vessels recognition](../master/Blood_vessels_recognition)
+
