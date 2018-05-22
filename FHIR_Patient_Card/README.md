f0b60f75d6520bacd0f7b6d08d992aa10dbce67d
Bartosz Górka <bartosz.gorka@appunite.com>
2018-05-22 21:57:07 +0200

    Init project

---
+++ FHIR_Patient_Card/README.md
@@ -0,0 +1,4 @@
+# FHIR Dashboard
+Simple FHIR dashboard with VueJS in action.
+
+Test server `http://hapi.fhir.org/baseDstu3`.
---
+++ FHIR_Patient_Card/src/constants/constant-paths.js
@@ -0,0 +1,7 @@
+const constPaths = () => {
+  return {
+    API_URL: 'http://hapi.fhir.org/baseDstu3/'
+  }
+}
+
+export default constPaths
